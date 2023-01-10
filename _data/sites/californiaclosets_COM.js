0453c66b503813d9860ba786c89d2e715791274b
mward85 <110117989+mward85@users.noreply.github.com>
2023-01-10 14:11:09 -0700

    Initial setup

---
+++ _data/sites/californiaclosets_COM.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: ".COM | California Closets", // optional, falls back to object key
+	description: "Most Popular pages for California Closets",
+	options: {
+		frequency: 60 * 23, // 23 hours
+	},
+	urls: [
+		"https://www.californiaclosets.com/",
+        // High Traffic Pages
+        "https://www.californiaclosets.com/pandora/",
+        "https://www.californiaclosets.com/home-office/"
+	]
+};
