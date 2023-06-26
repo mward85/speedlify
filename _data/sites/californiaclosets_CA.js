dd485c27f9ec1569af7867e8410729c4314f8700
mward85 <110117989+mward85@users.noreply.github.com>
2023-06-26 12:22:09 -0600

    Update Tracked Pages

---
+++ _data/sites/californiaclosets_CA.js
@@ -0,0 +1,35 @@
+module.exports = {
+	name: "California Closets .CA", // optional, falls back to object key
+	description: "Most Popular pages for California Closets Canada",
+	options: {
+		frequency: 60 * 23, // 23 hours
+	},
+	urls: [
+		"https://www.californiaclosets.ca/",
+        // High Traffic Pages",
+		"https://www.californiaclosets.ca/en/bedroom/murphy-beds/",
+		"https://www.californiaclosets.ca/en/",
+		"https://www.californiaclosets.ca/en/bedroom/walk-in-closets/",
+		"https://www.californiaclosets.ca/en/bedroom/custom-closets/finishes/",
+		"https://www.californiaclosets.ca/en/details/sliding-doors/",
+		"https://www.californiaclosets.ca/en/bedroom/custom-closets/closet-accessories/",
+		"https://www.californiaclosets.ca/fr/chambre-coucher/garde-robe-walk-in/",
+		"https://www.californiaclosets.ca/en/bedroom/reach-in-closets/",
+		"https://www.californiaclosets.ca/en/living-area/entertainment-centre/",
+		"https://www.californiaclosets.ca/en/living-area/entryways/",
+		"https://www.californiaclosets.ca/en/location/edmonton/murphy-bed-edmonton/",
+		"https://www.californiaclosets.ca/en/storage/laundry-rooms/",
+		"https://www.californiaclosets.ca/en/bedroom/custom-closets/",
+		"https://www.californiaclosets.ca/en/bedroom/wardrobe/",
+		"https://www.californiaclosets.ca/en/location/toronto/closet-design-toronto/",
+		"https://www.californiaclosets.ca/en/how-we-work/design-to-your-budget/decorative-hardware/",
+		"https://www.californiaclosets.ca/en/living-area/family-room/",
+		"https://www.californiaclosets.ca/en/living-area/mudrooms/",
+		"https://www.californiaclosets.ca/fr/chambre-coucher/penderie/",
+		"https://www.californiaclosets.ca/en/before-and-after/",
+		"https://www.californiaclosets.ca/en/living-area/",
+		"https://www.californiaclosets.ca/en/garage/storage-cabinets/",
+		"https://www.californiaclosets.ca/en/how-we-work/design-to-your-budget/",
+		"https://www.californiaclosets.ca/en/storage/laundry-rooms/laundry-room-accessories/
+	]
+};
